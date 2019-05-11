3dbcebac0a344eb0bf7c44b0e625486b219cdafe
Niwla23 <alwin.l@gmx.de>
2019-05-11 22:21:31 +0200

    Adding check if player is valid

--- init.lua
+++ init.lua
@@ -12,7 +12,10 @@ minetest.register_chatcommand("seeinfo", {
 	params = "<info>",
 	description = "Sets your info",
 	func = function(name, param)
-        local player = minetest.get_player_by_name(name)
+	local player = minetest.get_player_by_name(name)
+	if not player then
+		return false, "Player not found"
+	end
         local player2getinfo = minetest.get_player_by_name(param)
         minetest.chat_send_player(player:get_player_name(), player2getinfo:get_attribute("info"))
 	end,
