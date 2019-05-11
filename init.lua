8b9c5bf8cf042d56251c499dd44b0774b158473a
Niwla23 <alwin.l@gmx.de>
2019-05-11 21:33:06 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,19 @@
+minetest.register_chatcommand("setinfo", {
+	params = "<info>",
+	description = "Sets your info",
+	func = function(name, param)
+        local player = minetest.get_player_by_name(name)
+        player:set_attribute("info", param)
+		return true, "Set your info"
+	end,
+})
+
+minetest.register_chatcommand("seeinfo", {
+	params = "<info>",
+	description = "Sets your info",
+	func = function(name, param)
+        local player = minetest.get_player_by_name(name)
+        local player2getinfo = minetest.get_player_by_name(param)
+        minetest.chat_send_player(player:get_player_name(), player2getinfo:get_attribute("info"))
+	end,
+})
